fec302bb1027d036ee7396c97511cea965af4fb8
Sameer <sameer.libang@gmail.com>
2021-08-02 10:23:14 +1000

    api for transaction#show with passing test

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,13 +1,23 @@
 module Api
   module V1
     class TransactionsController < ApplicationController
+      before_action :set_transaction, only: :show
+
       def create 
         transaction = Transaction.create!(transaction_params)
         transaction.categorise
         render json: transaction.reload, status: :ok
       end
 
+      def show
+        render json: @transaction, status: :ok
+      end
+
       private
+
+      def set_transaction
+        @transaction = Transaction.find(params[:id])
+      end
 
       def transaction_params
         params.require(:transaction).permit(
